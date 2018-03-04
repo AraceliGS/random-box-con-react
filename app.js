0aca14090e646d0d822b61c13fc758d4ae391acd
AraceliGS <aragutarras@gmail.com>
2018-03-04 14:03:59 -0500

    añadiendo avances en la funcionalidad

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 
-   setInterval(function() {
-        const colors = ['black', 'red', 'green', 'blue'];
-        
-            var style = colors.map(function(el, index) {     
-                // debugger;
-                console.log(el, index)
-                if(index < colors.length) {
-                    let box = document.getElementById('random-box');
-                    box.classList = ' ';
-                    box.classList === el ? box.classList.add(colors[index + 1]) : box.classList.add(el);
-                    box.classList === 'blue' ? box.classList.add(colors[0]) : box.classList.add(el);
-                    // box.classList.add(el);
-                }
-            });
-   }, 3000);
-        // document.getElementsByClassName('box').classList.add(el);
-        // document.getElementByClassName('box').setAtrribute('id', index);
+//    setInterval(function() {
+//         // const colors = ['black', 'red', 'green', 'blue'];
+//         // const fontFamily = ['mina', 'roboto', 'lato', 'slato'];
+//         // const fontSize = ['2em', '4em', '1em', '3em'];
+//         // let box = document.getElementById('random-box');
+//         // var arr = [];
+//         // for(var i = 0; i < 3; i ++) {
+//         //     var num = Math.floor(Math.random() * 4);
+//         //     console.log(num)
+//         //     arr.push(num);
+//         // };
+//         // console.log(arr);
+//         // let backgroundColor = colors[arr[0]];
+//         // let font = fontFamily[arr[1]];
+//         // let letterSize = fontSize[arr[2]];
+//         // box.setAttribute('style', `background-color: ${backgroundColor}; font-family: ${font};  font-size: ${letterSize}`);
+//             // var style = colors.map(function(el, index) {     
+//             //     // debugger;
+//             //     console.log(el, index)
+//             //     if(index < colors.length) {
+//             //         let box = document.getElementById('random-box');
+//             //         box.classList = ' ';
+//             //         box.classList === el ? box.classList.add(colors[index + 1]) : box.classList.add(el);
+//             //         box.classList === 'blue' ? box.classList.add(colors[0]) : box.classList.add(el);
+//             //         // box.classList.add(el);
+//             //     }
+//             // });
+
+//         // var counter = 60;
+//         // for(var i = 60; -1 < i && i <= 60 ;i--) {
+//         //     // debugger;
+//         //     console.log(box.classList);
+//         //     counter -= 1;
+//         //     if(45 < i && i <= 60 === true) {
+//         //         box.classList = 'black';
+//         //     }
+
+//         //     if(30 < i && i <= 45 === true) {
+//         //         box.classList = 'red';
+//         //     } 
+//         //     if(15 < i && i <= 30 === true) {
+//         //         box.classList = 'green';
+//         //     }
+            
+//         //     if(-1 < i && i <= 15 === true) {
+//         //         box.classList = 'blue';
+//         //     }
+//         // }
+//    }, 2000);
+//         // document.getElementsByClassName('box').classList.add(el);
+//         // document.getElementByClassName('box').setAtrribute('id', index);
     
